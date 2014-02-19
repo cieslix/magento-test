fa9428c1a5af333244c9ee270a67e4cb5f978630
cieslix <cieslix@gmail.com>
2014-02-19 21:56:54 +0100

    pobranie kilku produktów

--- test.php
+++ test.php
@@ -2,3 +2,8 @@
 
 require_once './app/Mage.php';
 Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+foreach (Mage::getModel('catalog/product')->getCollection() as $product)
+{
+    Zend_Debug::dump($product->debug());
+}
