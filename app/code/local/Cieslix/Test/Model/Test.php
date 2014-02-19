be2487ea542ab7d34b5d1e70bb436a22206fe048
cieslix <cieslix@gmail.com>
2014-02-19 21:20:42 +0100

    Dodanie pustego modelu

---
+++ app/code/local/Cieslix/Test/Model/Test.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Test
+ *
+ * @author cieslix
+ */
+class Cieslix_Test_Model_Test
+{
+    //put your code here
+}
