f5c02ff0f5486339b940658c519913777ec2c3df
Paweł Cieślik <cieslix@gmail.com>
2014-03-24 21:00:10 +0100

    Story 182 Amend Ale

--- README.txt
+++ README.txt
@@ -2,3 +2,5 @@ magento-test
 ============
 
 magento test 23.03.2014
+
+24,03,2014
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once './app/Mage.php';
-Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
-
-foreach (Mage::getModel('catalog/product')->getCollection() as $product)
-{
-    Zend_Debug::dump($product->debug());
-}
